6245781016e1fa70448da77fce69aab91f18c1c2
AdminBVG <bvg@bvg.fin.ec>
2025-09-02 13:24:41 -0500

    refactor: adjust roles type

--- bvg-portal/src/app/core/constants/roles.ts
+++ bvg-portal/src/app/core/constants/roles.ts
@@ -1,7 +1,7 @@
 import rolesData from '../../../../../shared/roles.json';
 
-export const Roles = rolesData as const;
-export type Role = (typeof Roles)[keyof typeof Roles];
+export const Roles = rolesData;
+export type Role = keyof typeof Roles;
 export const ALLOWED_ASSIGNMENT_ROLES: Role[] = [
   Roles.AttendanceRegistrar,
   Roles.VoteRegistrar,
