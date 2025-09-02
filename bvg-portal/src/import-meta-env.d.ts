1da48069e22f96aed88496740305a43b7fc092e6
AdminBVG <bvg@bvg.fin.ec>
2025-09-02 09:28:36 -0500

    Add import meta env typings for Azure AD

---
+++ bvg-portal/src/import-meta-env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly NG_APP_AZURE_AD_CLIENT_ID: string;
+  readonly NG_APP_AZURE_AD_TENANT_ID: string;
+  readonly NG_APP_AZURE_AD_REDIRECT_URI: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
